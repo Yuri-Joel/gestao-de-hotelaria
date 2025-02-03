ac50232cfd2369fc1737d48e9c4c449754271904
Emerson Ely <57766767+EmersonEly@users.noreply.github.com>
2025-02-03 15:49:35 -0300

    Update page.tsx

--- src/app/(private)/[slug]/propriedades/page.tsx
+++ src/app/(private)/[slug]/propriedades/page.tsx
@@ -3,14 +3,13 @@ import { PropertiesList } from '@/components/propriedades/PropertiesList';
 import React from 'react';
 
 const page: React.FC = () => {
-    return (
-        <div className='h-screen bg-gray-300 flex items-center justify-center'>
-          <div className='bg-white w-[700px] h-full px-4 py-6'>
-            <Header/>
-            <PropertiesList/>
-          </div>
-        </div>
-    );
+  return (
+    <div className='h-screen bg-gray-300 flex items-center justify-center'>
+      <div className='bg-white w-[700px] h-full px-4 py-6'>
+        <PropertiesList />
+      </div>
+    </div>
+  );
 }
 
 export default page;
