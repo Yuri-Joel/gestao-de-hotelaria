f7f75804ca10e8c00921e1509750316c9f1c7938
Abraao-Daniel <danielabraao217@gmail.com>
2025-02-03 14:43:16 +0100

    up

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,9 +1,15 @@
+import Link from 'next/link';
 import React from 'react';
 
 const page: React.FC = () => {
+    const slug = "algo" 
     return (
         <div>
             <h1>Login</h1>
+            <Link href={`/${slug}/reservas`}>
+            Ir em reservas
+            </Link>
+        
         </div>
     );
 }
