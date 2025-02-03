071b83931100a6fbb9e25383deddd0cc936fdecc
Emerson Ely <57766767+EmersonEly@users.noreply.github.com>
2025-02-03 09:03:53 -0300

    Initial 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,26 @@
+'use client'
 import { Loading } from "@/components/Loading/Loading";
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from "react";
+import Cookies from 'js-cookie'
+import { delay } from "@/helpers/delay";
 
 export default function page() {
+  const router = useRouter()
+  const [property, setProperty] = useState("hotel-ao");
+
+  useEffect(() => {
+    ; (async function () {
+      const cookie = Cookies.get("pms_token")
+      await delay(2000);
+      if (cookie && property) {
+        router.push(`/${property}/home`)
+      } else {
+        router.push('/login')
+      }
+    })()
+  }, [property, router])
+
   return (
     <main className="flex h-screen items-center justify-center p-24">
       <Loading className="border-primary" />
---
+++ src/helpers/delay.ts
@@ -0,0 +1,1 @@
+export const delay = (amount = 1000) => new Promise(resolve => setTimeout(resolve, amount));
