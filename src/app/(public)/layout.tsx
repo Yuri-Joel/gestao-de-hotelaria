ea5680a92423548b6456fd31baa80fcc86945550
Emerson Ely <57766767+EmersonEly@users.noreply.github.com>
2025-02-03 15:01:12 -0300

    Update layout.tsx

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -3,7 +3,6 @@ import { Header } from "@/components/header/Header";
 const PublicLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="min-h-screen bg-white">
-      <Header />
       {children}
     </div>
   );
