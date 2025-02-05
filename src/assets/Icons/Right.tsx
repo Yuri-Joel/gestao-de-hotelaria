779c3643cd8a47e8dc8ca6e660286728e2dc8e6a
Abraao-Daniel <danielabraao217@gmail.com>
2025-02-05 15:51:56 +0100

    up

--- src/assets/Icons/Right.tsx
+++ src/assets/Icons/Right.tsx
@@ -10,9 +10,9 @@ export const Right: React.FC<Ticon> = ({ width, height, fill }) => {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="lucide lucide-check h-10 w-10 text-green-600"
     >
       <path d="M20 6 9 17l-5-5"></path>
--- src/assets/Icons/X.tsx
+++ src/assets/Icons/X.tsx
@@ -13,9 +13,9 @@ export const X: React.FC<Ticon> = ({ width, height, fill }) => {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             className="lucide lucide-x h-10 w-10 text-red-600"
             >
             <path d="M18 6 6 18"></path>
