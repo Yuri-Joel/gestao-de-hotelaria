cb93d3b598f257fb121fce4b47a74af67651d757
Emerson Ely <57766767+EmersonEly@users.noreply.github.com>
2025-02-06 13:42:32 -0300

    Delete page.tsx

--- src/app/(private)/[slug]/quartos/quartos-inicio/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Wrapper } from "@/components/Wrapper";
-import UnderDevelopmentMode from "@/components/UnderDevelopmentMode/UnderDevelopmentMode";
-
-const page: React.FC = () => {
-  return <Wrapper title="QUARTOS-INICIO" children={<UnderDevelopmentMode />} />;
-};
-
-export default page;
