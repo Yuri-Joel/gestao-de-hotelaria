f81f16045d78c3c86c71250b530d649b3da984b1
Emerson Ely <57766767+EmersonEly@users.noreply.github.com>
2025-02-07 05:32:40 -0300

    atualizações api fake

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -10,9 +10,7 @@ export async function GET(): Promise<NextResponse> {
 
         const data = JSON.parse(jsonData);
 
-        return NextResponse.json(data, {
-            status: 200,
-        });
+        return NextResponse.json({ data, status: 200 });
 
     } catch (error) {
         return NextResponse.json({ mensagem: 'Erro ao ler o arquivo.' }, {
