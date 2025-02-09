1e11d69ce87199333596ad23697c6f6f0d890b62
Emerson Ely <57766767+EmersonEly@users.noreply.github.com>
2025-02-09 09:56:34 -0300

    Update floorEntity.ts

--- src/interfaces/floorEntity.ts
+++ src/interfaces/floorEntity.ts
@@ -4,5 +4,8 @@ import { RoomEntity } from "./roomEntity"
 export interface FloorEntity  {
   _id: string
   title: string // titulo do andar
+  accessibility: string,
+  status: string ,
+  description: string ,
   rooms: RoomEntity[] // quartos vinculados ao andar
 }
