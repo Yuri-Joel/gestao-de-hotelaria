1c449ccf3e50bf77bed2737ab39e448310eeb8db
Emerson Ely <57766767+EmersonEly@users.noreply.github.com>
2025-02-11 07:09:33 -0300

    Revert "Update users.ts"
    This reverts commit feb23fcc8944644d343db8a3c85faa11d885948e.

--- src/services/users/users.ts
+++ src/services/users/users.ts
@@ -1,10 +1,10 @@
 import handleRequest from "@/helpers/handleRequest";
-import { UserEntity } from "@/interfaces/UserEntity";
+import { UsersEntity } from "@/interfaces/UsersEntity";
 import { TModelPagination } from "@/types/TModelPagination";
 
 export const usersServices = () => {
   const find = async (page: number) => {
-    const response = await handleRequest<TModelPagination<UserEntity>>({
+    const response = await handleRequest<TModelPagination<UsersEntity>>({
       url: `/users?page=${page}`,
       method: "GET",
     });
