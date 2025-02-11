68ed4957daae7c8c581982f3bed2bb2e4aa2b861
Emerson Ely <57766767+EmersonEly@users.noreply.github.com>
2025-02-11 15:09:40 -0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,3 +9,33 @@ export default function authMiddleware(request: NextRequest) {
 export const config = {
   matcher: ["/:path*"], // Aplica o middleware a todas as rotas
 };
+
+
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+// import { getAuthCookie } from "./helpers/cookies/authCookie";
+
+// export default function authMiddleware(request: NextRequest) {
+//   try {
+//     const { pathname } = new URL(request.url);
+//     const cookie = request.cookies.get("pms_data_user")?.value;
+//     const cookieParsed = cookie && JSON.parse(cookie);
+
+//     // Prevent infinite redirect loop
+//     if (pathname) {
+//       return NextResponse.next();
+//     }
+
+//     if (!cookieParsed) {
+//       return NextResponse.redirect(new URL("/", request.url));
+//     }
+
+//     return NextResponse.next();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// export const config = {
+//   matcher: ["/:path*"],
+// };
