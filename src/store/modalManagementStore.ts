563f375f2b7f92cd6eae26ffdb0840ca97698b6c
Yuri Joel <yurijoel.zeno@gmail.com>
2025-02-13 16:45:26 +0100

    Correções

--- src/store/modalManagementStore.ts
+++ src/store/modalManagementStore.ts
@@ -3,25 +3,17 @@ import { create } from 'zustand'
 
 type State = {
     isOpenedAlertDialogConfirmLogout: boolean
-    showModal: boolean
 }
 
 type Action = {
-    handleOpenAlertDialogConfirmLogout: () => void
-    setShowModal: (arg: boolean) => void
-  
+    handleOpenAlertDialogConfirmLogout: () => void  
 }
 
 export const modalManagementStore = create<State & Action>((set, get) => ({
         isOpenedAlertDialogConfirmLogout: false,
-        showModal: false,
         handleOpenAlertDialogConfirmLogout: () => {
             const isOpened = get().isOpenedAlertDialogConfirmLogout
             set({ isOpenedAlertDialogConfirmLogout: !isOpened })
-        },
-        setShowModal: (arg: boolean) => {
-            set({ showModal: arg})
         }
-
     }),
 )
