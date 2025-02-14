5e4174ffd2ad4fc8bc5ecbfd12946e728db067ee
Emerson Ely <57766767+EmersonEly@users.noreply.github.com>
2025-02-14 18:57:13 -0300

    Update page.tsx

--- src/app/(private)/[slug]/cadastro/quartos/page.tsx
+++ src/app/(private)/[slug]/cadastro/quartos/page.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-  import { Wrapper } from '@/components/Wrapper';
-import { UhList } from '@/components/quartos/Uhlist/uhlist';
+import { Wrapper } from '@/components/Wrapper';
+import { UhList } from '@/components/quartos/UhList/Uhlist';
 
 function page() {
   return (
-      <Wrapper
-        title='CADASTRO - Quartos'
-        description='Adicione, edite ou exclua os seus quartos'
-        children={<UhList />}
-      />
+    <Wrapper
+      title='CADASTRO - Quartos'
+      description='Adicione, edite ou exclua os seus quartos'
+      children={<UhList />}
+    />
   );
 }
 
