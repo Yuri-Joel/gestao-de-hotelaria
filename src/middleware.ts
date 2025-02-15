dcb81d432e379b9e47135594f5ee1e894c8f1abe
Emerson Ely <57766767+EmersonEly@users.noreply.github.com>
2025-02-15 05:54:52 -0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,41 +1,43 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
-export default function authMiddleware(request: NextRequest) {
-  // Apenas permite o fluxo sem redirecionamentos
-  return NextResponse.next();
-}
-
-export const config = {
-  matcher: ["/:path*"], // Aplica o middleware a todas as rotas
-};
-
-
 // import { NextResponse } from "next/server";
 // import type { NextRequest } from "next/server";
-// import { getAuthCookie } from "./helpers/cookies/authCookie";
 
 // export default function authMiddleware(request: NextRequest) {
-//   try {
-//     const { pathname } = new URL(request.url);
-//     const cookie = request.cookies.get("pms_data_user")?.value;
-//     const cookieParsed = cookie && JSON.parse(cookie);
-
-//     // Prevent infinite redirect loop
-//     if (pathname) {
-//       return NextResponse.next();
-//     }
-
-//     if (!cookieParsed) {
-//       return NextResponse.redirect(new URL("/", request.url));
-//     }
-
-//     return NextResponse.next();
-//   } catch (error) {
-//     console.log(error);
-//   }
+//   // Apenas permite o fluxo sem redirecionamentos
+//   return NextResponse.next();
 // }
 
 // export const config = {
-//   matcher: ["/:path*"],
+//   matcher: ["/:path*"], // Aplica o middleware a todas as rotas
 // };
+
+
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getAuthCookie } from "./helpers/cookies/authCookie";
+
+export default function authMiddleware(request: NextRequest) {
+  try {
+    const { pathname } = new URL(request.url);
+    const cookie = request.cookies.get("pms_data_user")?.value;
+    const slug = request.cookies.get("pms_slug")?.value;
+    const property = request.cookies.get("pms_ppt")?.value;
+    const cookieParsed = cookie && JSON.parse(cookie);
+
+    // Prevent infinite redirect loop
+    if (pathname) {
+      return NextResponse.next();
+    }
+
+    if (!cookieParsed || !slug || !property) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    return NextResponse.next();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const config = {
+  matcher: ["/:path*"],
+};
